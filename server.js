fc9883b985bbcd2da96dab487fa88e9dab1a9090
Abbashozefa <abbashozefa@gmail.com>
2023-01-19 22:18:18 +0530

    sd

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const {readFileSync,writeFileSync} = require('fs');
+
 const express = require('express');
-const path = require('path')
+
+const path = require('path');
+
 const res = require('express/lib/response');
 const app = express();
-
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'/index.html'))
 
